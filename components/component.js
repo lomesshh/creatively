6cffec10d40487e4193618a3691b82dc1aa33d4d
lomesh <badhelomesh82@gmail.com>
2022-01-28 17:33:16 +0530

    Feat: rating-toast

--- components/component.js
+++ components/component.js
@@ -1,3 +1,6 @@
+
+// Model Javascript 
+
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
@@ -16,3 +19,18 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+// Toast Javascript
+
+
+
+var toastBtn = document.getElementsByClassName("toast")[0];
+
+  toastBtn.onclick = function () {
+    var x = document.getElementById("toast-message");
+    x.className = "show";
+    setTimeout(function () {
+    x.className = x.className.replace("show", "");
+    }, 3000);
+  };
